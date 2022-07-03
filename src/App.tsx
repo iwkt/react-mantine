1cdfd1a0ee53d76e1851c0316a71cc4247b466a4
iwkt <iwk@mac.com>
2022-07-03 21:01:34 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from '@mantine/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Hello world!</Button>
+    </div>
+  );
+}
+
+export default App;
